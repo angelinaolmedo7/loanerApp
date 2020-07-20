a24c8abb1f3e1376fd463a9937f0f80e8bb17639
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2020-07-20 09:44:50 -0700

    Created the managed object

---
+++ LoanerApp/Loaner/Model/Item+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Item+CoreDataProperties.swift
+//  loaner
+//
+//  Created by Angelina Olmedo on 7/20/20.
+//  Copyright © 2020 LinnierGames. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+import UIKit.UIImage
+
+extension Item {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Item> {
+        return NSFetchRequest<Item>(entityName: "Item")
+    }
+
+    @NSManaged public var notes: String
+    @NSManaged public var itemImage: UIImage
+    @NSManaged public var itemTitle: String
+    @NSManaged public var loanee: Loanee
+
+}
